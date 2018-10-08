fb970aea28229f49b3715130d48a07830cf30087
侯顺发 <857816551@qq.com>
2018-10-08 17:11:47 +0800

    测试

--- app/src/main/java/com/cc/notes/test.java
+++ app/src/main/java/com/cc/notes/test.java
@@ -11,6 +11,8 @@ public class test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
+/*
+* 发提交后
+* */
     }
 }
