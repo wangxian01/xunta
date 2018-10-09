7e05df955f4ebdca74da928baf73e059b101dabd
815380884 <815380884@qq.com>
2018-10-09 14:49:26 +0800

    随便提交

---
+++ app/src/main/java/com/cc/notes/ui/activity/HomeSecondActivity.java
@@ -0,0 +1,15 @@
+package com.cc.notes.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.notes.cc.notes.R;
+
+public class HomeSecondActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondpaper_layout);
+
+
+    }
+}
--- app/src/main/java/com/cc/notes/ui/activity/MainActivity.java
+++ app/src/main/java/com/cc/notes/ui/activity/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     private void inint() {
         lists= new ArrayList<>();
         for(String string : strings){
